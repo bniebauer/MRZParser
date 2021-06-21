e5e0cd6fb7370cc0718f68a9cbba50b62f1c0f81
Brenton Niebauer <b.niebauer@me.com>
2021-06-21 16:35:46 -0400

    Initial Commit

---
+++ MRZParserApp/MRZParserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRZParserAppApp.swift
+//  MRZParserApp
+//
+//  Created by Brenton Niebauer on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MRZParserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
